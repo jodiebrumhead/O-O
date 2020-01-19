7184a2a54b7a729044b2a2b3a48921ada18e396a
jodiebrumhead <43830291+jodiebrumhead@users.noreply.github.com>
2020-01-19 18:59:41 +0000

    Sort algorithm finished and outputs added to dataframe

--- sort.py
+++ sort.py
@@ -1,95 +1,116 @@
 """Timing sorting algorithms on different 1D data structures"""
 
-#import modules
+# import modules
 from timeit import default_timer as timer
 import random
 import numpy as np
 import pandas as pd
 
 """Create lists, arrays and series to compare"""
+
+
 def create(n):
     ls = []
-    for i in range(n):
+    for j in range(n):
         num = random.random()
         ls.append(num)
-    arr=np.random.random(n)
-    ser=pd.Series(np.random.random(n))
+    arr = np.random.random(n)
+    ser = pd.Series(np.random.random(n))
     return ls, arr, ser
 
-"""A find mimimum function created by me"""
+
+"""A find minimum function created by me"""
+
+
 def findmin(obj):
-    minN = 10000 # this being the key
+    minn = 10000  # this being the key
     for i in obj:
-        if (i<minN):
-            minN=i
-    return minN
-#could create find max in similar manner
-#beware of -values?
+        if i < minn:
+            minn = i
+    return minn
+
+
+# could create find max in similar manner
+# beware of -values?
 
 """A sort function created by me"""
-#'for index in range(len((arr))' different to 'for i in arr'
+
+
+# 'for index in range(len((arr))' different to 'for i in arr'
 # is it best to find index at same stage as finding min?
 # i.e. var = and pos =
 # test with duplicate values!
 def sort(obj):
     new = []
-    while len(obj)>0:
+    while len(obj) > 0:
         b = findmin(obj)
         new.append(b)
         if type(obj) is list:
-            obj.remove(b) #removes first item of value b
+            obj.remove(b)  # removes first item of value b
         if type(obj) is np.ndarray:
-            index = np.argwhere(obj==b)
-            obj = np.delete(obj, [index]) #must equal something else as np array immutable
+            index = np.argwhere(obj == b)
+            obj = np.delete(obj, [index])  # must equal something else as np array immutable
         if type(obj) is pd.core.series.Series:
             pos = obj[obj == b].index[0]
             obj = obj.drop(labels=pos)
-        else:
-            break
     return new
 
+
 if __name__ == '__main__':
-# Use create function to create random 10 values in different data structures
+
+    # Use create function to create random 10 values in different data structures
     ls, arr, ser = create(10)
     datastruc10 = [ls, arr, ser]
 
-"""#Create dataframe and add data
-"""
-# create empty dataframe with index for different 1d data structures
-times = pd.DataFrame(index=['python list', 'numpy array', 'pandas series'])
+    """Create dataframe and add data"""
+    # create empty dataframe with index for different 1d data structures
+    times = pd.DataFrame(index=['python list', 'numpy array', 'pandas series'])
+    times['listobj'] = datastruc10
 
-m = []
-time = []
-for i in datastruc10:
-    s = timer()
-    m.append(findmin(i))
-    e = timer()
-    time.append(e - s)
-#Create list of code strings? anyway to automate this?
+    m = []
+    time = []
+    for i in datastruc10:
+        s = timer()
+        m.append(findmin(i))
+        e = timer()
+        time.append(e - s)
 
-times['listobj'] = datastruc10
-times['mymin'] = m
-times['mytime'] = time
+    times['mymin'] = m
+    times['mytime'] = time
+
+    # Use in-built functions relevant for different data structures and time
+    # then add to dataframe
+    start = timer()
+    ls_min = min(ls)
+    end = timer()
+    ls_findmin = (end - start)
+    start = timer()
+    arr_min = np.amin(arr)
+    end = timer()
+    arr_findmin = (end - start)
+    start = timer()
+    ser_min = ser.min()
+    end = timer()
+    ser_findmin = (end - start)
+
+    times['minfunc'] = [ls_min, arr_min, ser_min]
+    times['minfunctime'] = [ls_findmin, arr_findmin, ser_findmin]
 
 
-# Use in-built functions relevant for different data structures and time
-# could function this?
-start = timer()
-ls_min = min(ls)
-end = timer()
-ls_findmin = (end - start)
-start = timer()
-arr_min = np.amin(arr)
-end = timer()
-arr_findmin = (end - start)
-start = timer()
-ser_min = ser.min()
-end = timer()
-ser_findmin = (end - start)
+    sortedobj = []
+    time = []
+    for i in datastruc10:
+        s = timer()
+        sortedobj.append(sort(i))
+        e = timer()
+        time.append(e - s)
+    # Create list of code strings? anyway to automate this?
 
-times['minfunc'] = [ls_min, arr_min, ser_min]
-times['minfunctime'] = [ls_findmin, arr_findmin, ser_findmin]
+    times['mysort'] = sortedobj
+    times['mysorttime'] = time
 
-#Output dataframe to csv
+    print(times)
 
-times.to_csv(r'times.csv')
+
+    # Output dataframe to csv
+    times.to_csv(r'times.csv')
